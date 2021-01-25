0c8345efa0e013d0ec98927370a1abca7f897b05
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-25 20:04:55 +0530

    Update aws-lex-audio.js

--- aws-lex-audio.js
+++ aws-lex-audio.js
@@ -471,11 +471,12 @@ function playOutput(base64_string){
 
   var dialog = function(state,text_msg,audio_file_url) {
     //var jwt = getJWT();
-    const url = 'https://dialogflow.googleapis.com/v2beta1/projects/'+PROJECT_ID+'/agent/sessions/123456:detectIntent?key='+API_KEY
+    var date = new Date();
+    const url = 'https://dialogflow.googleapis.com/v2beta1/projects/'+PROJECT_ID+'/agent/sessions/'+state.config.profile.ID+':detectIntent?key='+API_KEY
     const data = {
         "queryInput": {
            "text": {"text": text_msg,
-                    "languageCode":  LANG_CODE
+                    "languageCode": LANG_CODE 
                    }
         },
        "queryParams":{
@@ -484,7 +485,10 @@ function playOutput(base64_string){
     		'userID': state.config.profile.ID,
     		'Name': state.config.profile.Name,
         	'audioURL': audio_file_url,
-        	'textmsg': text_msg
+        	'textmsg': text_msg,
+        	'action':  localStorage.getItem("action"),
+        	'timestring': date.toDateString(),
+        	'datestring': date.getUTCHours() + date.getUTCMinutes(),
   	}
        }
      };
@@ -525,126 +529,29 @@ function playOutput(base64_string){
 
   }
   function displayTip(state,action){
+    localStorage.setItem("action",action);
     if(action == "input.welcome"){
         var tip = document.getElementById('tip');
-        tip.textContent = 'TIP to say something: ask tables OR ask me tables upto 15';
+        tip.textContent = 'say \"ask 12 table\" OR \"ask tables upto 15\"';
     }
     if(action == "askTables"){
         var tip = document.getElementById('tip');
-        tip.textContent = 'TIP to say something: ok OR yes OR fine';
+        tip.textContent = 'say \"ok\" OR \"yes\" OR \"fine"';
     } 
     if(action == "askMultiplicationAction"){
         var tip = document.getElementById('tip');
-        tip.textContent = 'TIP to say something: 20 OR i dont know OR skip';
+        tip.textContent = 'say \"10\" OR \"i dont know\" OR \"skip\"';
     }   
     if(action == "tryAnswerAgainAction"){
         var tip = document.getElementById('tip');
-        tip.textContent = 'TIP to say something: 20 OR i dont know OR skip';
+        tip.textContent = 'say \"10\" OR \"i dont know\" OR \"skip\"';
     }       
     if(action == "input.unknown"){
         var tip = document.getElementById('tip');
-        tip.textContent = 'TIP to say something: 20 OR i dont know OR skip OR start over';
+        tip.textContent = 'say \"20\" OR \"i dont know\" OR \"skip\" OR \"start over\"';
     }               
   }
-function postJWT(jwt, callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200 && callback) {
-                callback(this.responseText);
-                console.log(this.responseText);
-                return;
-            }
-            if (console) console.log(this.responseText);
-        }
-    };
-    var parameters = "grant_type=" + encodeURIComponent("urn:ietf:params:oauth:grant-type:jwt-bearer") + "&assertion=" + encodeURIComponent(jwt);
-    xhttp.open("POST", "https://www.googleapis.com/oauth2/v4/token", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send(parameters);
-}
 
-function getCert() {
-    var cert = //your json key (downloaded earlier) goes here
-     {
-        "type": "service_account",
-  	"project_id": PROJECT_ID,
-  	"private_key_id": PRIVATE_KEY_ID,
-  	"private_key": PRIVATE_KEY,
-  	"client_email": CLIENT_EMAIL,
-  	"client_id": CLIENT_ID,
-  	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  	"token_uri": "https://oauth2.googleapis.com/token",
-  	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  	"client_x509_cert_url": CLIENT_X509_CERT_URL
-     };      
-    return cert;
-}
-function getJWT() {
-
-    var imported = document.createElement('script');
-    imported.src = 'jsrsasign-all-min.js';
-    document.head.appendChild(imported);
-
-    var cert = getCert();
-    var key = KEYUTIL.getKey(cert.private_key);
-    var headers = { "alg": "RS256", "typ": "JWT" };
-    var issued = Math.floor(new Date().getTime()/1000);
-
-    var claims = {
-        "iss": cert.client_email,
-        "scope": "https://www.googleapis.com/auth/dialogflow",
-        "aud": "https://www.googleapis.com/oauth2/v4/token",
-        "exp": issued + 3600,
-        "iat": issued
-    };
-
-    var jwt = KJUR.jws.JWS.sign(headers.alg, headers, JSON.stringify(claims), key);
-    return jwt;
-}
-
-function jsonToStructProto(json) {
-  const fields = {};
-  for (let k in json) {
-    fields[k] = jsonValueToProto(json[k]);
-  }
-
-  return {fields};
-}
-
-const JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP = {
-  [typeof 0]: 'numberValue',
-  [typeof '']: 'stringValue',
-  [typeof false]: 'boolValue',
-};
-
-const JSON_SIMPLE_VALUE_KINDS = new Set([
-  'numberValue',
-  'stringValue',
-  'boolValue',
-]);
-
-function jsonValueToProto(value) {
-  const valueProto = {};
-
-  if (value === null) {
-    valueProto.kind = 'nullValue';
-    valueProto.nullValue = 'NULL_VALUE';
-  } else if (value instanceof Array) {
-    valueProto.kind = 'listValue';
-    valueProto.listValue = {values: value.map(jsonValueToProto)};
-  } else if (typeof value === 'object') {
-    valueProto.kind = 'structValue';
-    valueProto.structValue = jsonToStructProto(value);
-  } else if (typeof value in JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP) {
-    const kind = JSON_SIMPLE_TYPE_TO_PROTO_KIND_MAP[typeof value];
-    valueProto.kind = kind;
-    valueProto[kind] = value;
-  } else {
-    console.warn('Unsupported value type ', typeof value);
-  }
-  return valueProto;
-}
 
   var Sending = function(state) {
     this.state = state;
